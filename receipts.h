00b0f4ba18190eb2fd88f256bff6c0bac10a028e
Benjamin Toll <benjam72@yahoo.com>
2017-03-23 15:21:11 -0400

    Added receipts.h

---
+++ receipts.h
@@ -0,0 +1,14 @@
+void add_receipt(sqlite3 *db);
+void add_store(sqlite3 *db);
+void *build_cols(char *s, char *q);
+void clear(void);
+sqlite3 *get_db(void);
+int has_rows(sqlite3 *db);
+void query(sqlite3 *db);
+void required(char *field, char *buf);
+void strip_newline(char *buf);
+
+#define QUERY_MAX 150
+#define VALUE_MAX 50
+#define IS_DIGIT(N) ((N) >= '0' && (N) <= '9')
+
